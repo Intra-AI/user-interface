9753c25b2680d1cf2b492ebcee86535c6ecdd99e
Intra AI IONOS Unix Server <info@intra-ai.de>
2025-11-21 15:40:59 +0100

    change labelling, modify vision model handling

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -58,8 +58,8 @@ export default defineConfig(({ command }) => ({
       },
       includeAssets: [],
       manifest: {
-        name: 'LibreChat',
-        short_name: 'LibreChat',
+        name: 'Intra AI',
+        short_name: 'Intra AI',
         display: 'standalone',
         background_color: '#000000',
         theme_color: '#009688',
