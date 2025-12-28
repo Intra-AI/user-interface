71b50214c773ee0c2884ee28a2dbed4bc34c202e
intra-vps <info@intra-ai.de>
2025-12-28 12:49:31 +0100

    Change Storage Limit

--- api/server/utils/storage.js
+++ api/server/utils/storage.js
@@ -1,5 +1,5 @@
 // Default storage limit: 5GB in bytes (use 104857 for 0.1MB testing)
-const DEFAULT_STORAGE_LIMIT = 104857; // 0.1MB for testing - change to 5368709120 for 5GB
+const DEFAULT_STORAGE_LIMIT = 5368709120; // 0.1MB for testing - change to 5368709120 for 5GB
 
 /**
  * Get the storage limit from config or use default
